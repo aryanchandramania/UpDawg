36c71a3d8e10573ebb3965c5415a1a6d75724d2a
aryanchandramania <arashaurchandramania@gmail.com>
2024-04-22 19:44:47 +0530

    integrate, clean up

--- DataPull/Slack/SlackDataPull.py
+++ DataPull/Slack/SlackDataPull.py
@@ -1,100 +1,125 @@
-import os
 from slack_sdk import WebClient 
 from slack_sdk.errors import SlackApiError 
 import json
 import datetime
+import configparser
+import asyncio
 
-""" We need to pass the 'Bot User OAuth Token' """
-slack_token = 'xoxb-6909856229971-6912673347860-qclXgwLwCsjKLfycFF9J788f'
+import sys
+sys.path.append('..')
+from DataPull import DataPull
 
-# Creating an instance of the Webclient class
-client = WebClient(token=slack_token)
+class SlackDataPull(DataPull):    
+    def __init__(self):
+        config = configparser.ConfigParser()
+        config.read('config.cfg')
+        self.slack_token = config['SLACK']['SLACK_TOKEN']
+        self.client = WebClient(token=self.slack_token)	
+        self.id_to_channel = {}
+        self.id_to_user = {}
+        self.history_json = {}
 
-def convert_ts_to_date(ts):
-	return datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    def convert_ts_to_date(self, ts):
+        return datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
 
-def too_old(ts, time_delta):
-	return datetime.datetime.now().timestamp() - float(ts) > time_delta * 60 * 60 * 24
+    def too_old(self, ts, time_delta,):    
+        return datetime.datetime.now().timestamp() - float(ts) > time_delta * 60 * 60 * 24
+    
+    def nzprint(self, text, value, ztext="results"):
+        if value != 0:
+            print(text)
+        else:
+            print("No ", ztext, " found.")
 
-id_to_channel = {}
-def get_channel(id):
-	if id in id_to_channel:
-		return id_to_channel[id]
-	return "Unknown channel"
+    def get_channel(self, id):
+        if id in self.id_to_channel:
+            return self.id_to_channel[id]
+        return "Unknown channel"
 
-id_to_user = {}
-def get_user(id):
-	if id in id_to_user:
-		return id_to_user[id]
-	return "Unknown user"
+    def get_user(self, id):
+        if id in self.id_to_user:
+            return self.id_to_user[id]
+        return "Unknown user"
 
-def post_message(text, channel="random"):
-	try:
-		response = client.chat_postMessage(
-						channel=channel,
-						text=text)
-	except SlackApiError as e:
-		print(e)
-  
-def send_message_to_user(text, user_id="U06T4JLQHQR", channel="random"):
-	try:
-		response = client.chat_postEphemeral(
-						channel=channel, 
-						text=text, 
-						user=user_id)
-	except SlackApiError as e:
-		print(e)
-  
-history_json = {}
+    def post_message(self, text, channel="random"):
+        try:
+            response = self.client.chat_postMessage(
+							channel=channel,
+							text=text)
+        except SlackApiError as e:
+            print(e)
+	
+    def send_message_to_user(self, text, user_id="U06T4JLQHQR", channel="random"):
+        try:
+            response = self.client.chat_postEphemeral(
+							channel=channel, 
+							text=text, 
+							user=user_id)
+        except SlackApiError as e:
+            print(e)
+	
+    def isProviderAlive(self):
+        try:
+            response = self.client.api_test()
+            print("Provider is alive")
+        except SlackApiError as e:
+            print(e)
 
-def get_conv_history(time_delta):
-	try:  
-		# Get a list of all channels
-		print("List of all channels: ")
-		print("CHANNEL ID\tCHANNEL NAME")
-		channels_list = client.conversations_list()
-		for channel in channels_list['channels']:
-			id_to_channel[channel['id']] = channel['name']
-			print(channel['id'], '\t', channel['name'], '\t',)
-			history_json[channel['name']] = []
-		print('\n')
-  
-		print("There are ", len(id_to_channel), " channels in the workspace.")
-		for channel_id in id_to_channel.keys():
-			# Get the members of the conversation
-			print("Channel: ", id_to_channel[channel_id])
-			conv_members = client.conversations_members(
-							channel=channel_id)
-			print(len(conv_members['members']), " members in the conversation: ")
+    async def pullData(self, time_delta):
+        try:  
+            # Get a list of all channels
+            channels_list = self.client.conversations_list()
+            self.nzprint("List of all channels:\nCHANNEL ID\tCHANNEL NAME", len(channels_list['channels']), "channels")
+            for channel in channels_list['channels']:
+                self.id_to_channel[channel['id']] = channel['name']
+                print(channel['id'], '\t', channel['name'], '\t',)
+                self.history_json[channel['name']] = []
+            print('\n')
+        
+            self.nzprint("There are " + str(len(self.id_to_channel)) + " channels in the workspace.", len(self.id_to_channel), "channels")
+            for channel_id in self.id_to_channel.keys():
+                try:
+                    # Get the members of the conversation
+                    print("Channel: ", self.id_to_channel[channel_id])
+                    conv_members = self.client.conversations_members(
+                                    channel=channel_id)
+                    self.nzprint(str(len(conv_members['members'])) + " members in the conversation: ", len(conv_members['members']), "members")
 
-			# Get the names of the members
-			for member in conv_members['members']:
-				if member in id_to_user:
-					continue
-				response = client.users_info(
-								user=member)
-				id_to_user[member] = response['user']['real_name']
-				print(id_to_user[member])
-			print()
+                    # Get the names of the members
+                    for member in conv_members['members']:
+                        if member not in self.id_to_user:
+                            response = self.client.users_info(
+                                            user=member)
+                            self.id_to_user[member] = response['user']['real_name']
+                        print(self.id_to_user[member])
+                    print()
 
-			# Get the conversation history
-			print("Messages in the conversation: ")
-			response = client.conversations_history(
-							channel=channel_id)
-			for message in response['messages'][::-1]:
-				if too_old(message['ts'], time_delta):
-					continue
-				for user_id in id_to_user.keys():
-					message['text'] = message['text'].replace(f'<@{user_id}>', id_to_user[user_id])
-				print(f'{get_user(message["user"])} [{convert_ts_to_date(float(message['ts']))}]: {message["text"]}')
-				history_json[id_to_channel[channel_id]].append({'user': get_user(message['user']), 'time': convert_ts_to_date(float(message['ts'])), 'text': message['text']})
-			print()
-		print()
-		with open('history.json', 'w') as f:
-			json.dump(history_json, f)
-		
-	except SlackApiError as e:
-		print(e)
+                    # Get the conversation history
+                    response = self.client.conversations_history(
+                                    channel=channel_id)
+                    newStuff = False
+                    for message in response['messages'][::-1]:
+                        if not self.too_old(float(message['ts']), time_delta):
+                            newStuff = True
+                            break
+                    self.nzprint("Messages in the conversation: ", 1 if newStuff else 0, "new messages")
+                    for message in response['messages'][::-1]:
+                        if self.too_old(message['ts'], time_delta):
+                            continue
+                        for user_id in self.id_to_user.keys():
+                            message['text'] = message['text'].replace(f'<@{user_id}>', self.id_to_user[user_id])
+                        print(f'{self.get_user(message["user"])} [{self.convert_ts_to_date(float(message['ts']))}]: {message["text"]}')
+                        self.history_json[self.id_to_channel[channel_id]].append({'user': self.get_user(message['user']), 'time': self.convert_ts_to_date(float(message['ts'])), 'text': message['text']})
+                    print()
+                except:
+                    print("The bot has not been added to channel ", self.id_to_channel[channel_id])
+            # with open('history.json', 'w') as f:
+            #     json.dump(self.history_json, f)
+            
+        except SlackApiError as e:
+            print(e)
   
 if __name__ == "__main__":
-	get_conv_history(1)
+    slack = SlackDataPull()
+    slack.isProviderAlive()
+    asyncio.run(slack.pullData(1))
