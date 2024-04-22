45449c505ba9e06b4ab315d7740c585e0a21aa0b
raghav <raghavdonakanti@gmail.com>
2024-04-22 23:21:24 +0530

    added Data engine key functions

--- DataEngine/DataEngine.py
+++ DataEngine/DataEngine.py
@@ -1,3 +1,12 @@
-def checkGap(time):
-    
-def getDataFromDB():
+import sys
+sys.path.append('..')
+from Database import message_dao
+
+class DataEngine:
+    def __init__(self):
+
+    def pushData(self):
+
+    def checkGap(self):
+        
+    def getDataFromDB(self):
